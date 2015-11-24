faafd94acf5503cb75efed0261c6fddbaaeb402a
Anni <annelise.pronschinske@cuw.edu>
2015-11-24 10:08:59 -0600

    HW Assignment #14

--- JSONArray.java
+++ JSONArray.java
@@ -1,33 +1,6 @@
-public class JSONArray extends JSONVariable
+public class JSONArray 
 {
-	private JSONArray[] ObjectArray; 
-	private int currSize; 
 	
-	
-	public JSONArray() 
-	{
-		this.ObjectArray = new JSONArray[10];
-		this.currSize = 0; 
-		
-	}
-	public void addVariable(JSONVariable OA)
-	{
-		if(this.currSize < this.ObjectArray.length)
-		{
-			this.ObjectArray[this.currSize] = OA;
-			this.currSize++;
-			
-		}
-		
-	public void display()
-	{
-		System.out.println("JSON Array - Num Objects: " + this.currSize);
-		for(int i = 0; i < this.currSize; i++)
-			{
-				this.ObjectArray[i].display();
-			}
-			
-	}
 }
 
 
